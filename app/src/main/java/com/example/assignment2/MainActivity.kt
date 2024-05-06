9c201c6e0e3ae76a0d6c0253f683521b281f0456
luxolosobetwa <st10443381@vcconnect.edu.za>
2024-05-06 11:27:21 +0200

    Commit1

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+        // MainActivity.kt
+
+                val getStartedButton: Button = findViewById(R.id.getStartedButton)
+                getStartedButton.setOnClickListener {
+
+                }
+            }
+        }
